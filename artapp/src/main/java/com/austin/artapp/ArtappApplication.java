4a4a07725384034cd629166ad1967a645ef46789
Austin Nye <amnye0815@gmail.com>
2022-02-08 14:05:27 -0800

    inital application framework

---
+++ artapp/src/main/java/com/austin/artapp/ArtappApplication.java
@@ -0,0 +1,13 @@
+package com.austin.artapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtappApplication.class, args);
+	}
+
+}
